614941374321d32f1c7139c5b9feadba18f8420e
nbrazus <nathanbrazus@gmail.com>
2026-06-24 20:54:59 +0000

    start

---
+++ src/textnode.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
